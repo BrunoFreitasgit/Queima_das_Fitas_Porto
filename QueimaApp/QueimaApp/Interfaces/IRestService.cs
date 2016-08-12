3084eefa1ff5c6c8859a71497ab0a92a1d4afc28
BrunoFreitas <1130504@isep.ipp.pt>
2016-08-12 16:36:34 +0100

    Updates aos packages

---
+++ QueimaApp/QueimaApp/Interfaces/IRestService.cs
@@ -0,0 +1,21 @@
+﻿using QueimaApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueimaApp.Interfaces
+{
+    public interface IRestService
+    {
+        Task<List<Transporte>> TransportesRefreshAsync();
+
+        Task<List<Artista>> ArtistasRefreshAsync();
+
+        Task<List<AtividadeAcademica>> AtividadesRefreshAsync();
+
+        Task<List<Bilheteira>> BilheteiraRefreshAsync();
+
+    }
+}
---
+++ QueimaApp/QueimaApp/Models/Transporte.cs
@@ -0,0 +1,35 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueimaApp.Models
+{
+    [ImplementPropertyChanged]
+    public class Transporte
+    {
+        public int Id { get; set; } 
+        // Nome do Tipo de Transporte (STCP, Metro ou Taxi)
+        public TipoTransporte Nome { get; set; }
+        // Link para informações adicionais
+        public string UrlBilheteiraOnline { get; set; }
+        // Descrição do serviço de Transporte
+        public string Descricao { get; set; }
+        // Imagem descritiva do Transporte
+        public string ImagemUrl { get; set; }
+        public Transporte()
+        {
+
+        }
+    }
+
+    // Tipos de Transporte 
+    public enum TipoTransporte
+    {
+        STCP,
+        Metro,
+        Taxi,
+    }
+}
